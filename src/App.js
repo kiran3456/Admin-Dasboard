f160214672d6054bb615218d2f3e7063f56d1b70
Kiran <kirandeepkaurfdk5@gmail.com>
2023-07-03 16:08:46 -0800

    bar chart added

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
+import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 function App() {
@@ -31,11 +31,11 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
 
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path="/bar" element={<Bar />} /> */}
+              <Route path="/bar" element={<Bar />} />
               <Route path="/form" element={<Form />} />
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
+              <Route path="/faq" element={<FAQ />} />
               {/* <Route path="/geography" element={<Geography />} /> */}
               <Route path="/calendar" element={<Calendar />} />
             </Routes>
--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -123,42 +123,6 @@ const Calendar = () => {
             ]}
           />
         </Box>
-        {/* <Box flex="1 1 100%" ml="15px">
-          <FullCalendar
-            height="75vh"
-            plugins={[
-              dayGridPlugin,
-              timeGridPlugin,
-              interactionPlugin,
-              listPlugin,
-            ]}
-            headerToolbar={{
-              left: "prev,next today",
-              center: "title",
-              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
-            }}
-            initialView="dayGridMonth"
-            editable={true}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={true}
-            select={handleDateClick}
-            eventClick={handleEventClick}
-            eventsSet={(events) => setCurrentEvents(events)}
-            initialEvents={[
-              {
-                id: "1234",
-                title: "All-day event",
-                date: "2022-09-14",
-              },
-              {
-                id: "4321",
-                title: "Timed event",
-                date: "2022-09-28",
-              },
-            ]}
-          />
-        </Box> */}
       </Box>
     </Box>
   );
